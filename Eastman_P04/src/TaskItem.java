c693df3c004e5a0b3df29cf2466c1ffead4aa345
caseeastman1 <70347691+caseeastman1@users.noreply.github.com>
2020-11-16 20:12:41 -0500

    Update TaskItem.java

--- Eastman_P04/src/TaskItem.java
+++ Eastman_P04/src/TaskItem.java
@@ -1,5 +1,9 @@
 
 
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class TaskItem {
     private String title;
     private String description;
@@ -67,9 +71,6 @@ public class TaskItem {
     }
 
 }
-
-
-
 
 
 
