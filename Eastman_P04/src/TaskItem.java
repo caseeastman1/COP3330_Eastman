2b7f5771fa8b8fa401ed1a8901b16870132eaf1c
caseeastman1 <70347691+caseeastman1@users.noreply.github.com>
2020-11-16 20:37:08 -0500

    Add files via upload

--- Eastman_P04/src/TaskItem.java
+++ Eastman_P04/src/TaskItem.java
@@ -1,9 +1,3 @@
-
-
-import org.junit.Test;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 public class TaskItem {
     private String title;
     private String description;
@@ -71,8 +65,3 @@ public class TaskItem {
     }
 
 }
-
-
-
-
-
