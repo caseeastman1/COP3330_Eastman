f1544fd2e7a6be29acd948fa88ada5be8f9b8f40
case <caseeastman@hotmail,com>
2020-11-20 16:48:31 -0500

    Change

--- Eastman_P5/src/TaskItem.java
+++ Eastman_P5/src/TaskItem.java
@@ -1,2 +1,57 @@
+import java.time.format.DecimalStyle;
+
 public class TaskItem {
+    private String title;
+    private String description;
+    private String date;
+    public String s;
+    private boolean isCompleted  = false;
+
+
+
+
+    public TaskItem(String title, String description, String date, boolean isCompleted) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.isCompleted = isCompleted;
+    }
+
+    public TaskItem(String title,String description, String date) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.isCompleted = false;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
